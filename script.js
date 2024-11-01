dbd6d3c5cf5975665e7b91c14e038483992f1150
roydilip955 <roydilip955@gmailcom>
2024-11-01 20:03:07 +0530

    index

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-function submitMood(event) {
-    event.preventDefault(); // Prevent the form from submitting normally
-    const mood = document.getElementById("mood").value;
-    window.location.href = `recommendation.html?mood=${mood}`; // Redirect with mood as a URL parameter
+function selectMood(mood) {
+    window.location.href = `recommendation.html?mood=${mood}`;
 }
